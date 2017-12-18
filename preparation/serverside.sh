c92d654a0a8729e3bb47e0dc4fdf21ae06c3a01f
atsttk84 <atsttk84@gmail.com>
2017-12-19 04:09:22 +0900

    add python install

--- preparation/serverside.sh
+++ preparation/serverside.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+mkdir -p ~/.ssh
 wget -O ~/.ssh/authorized_keys https://raw.githubusercontent.com/atsttk84/ansible/master/tempkey/id_ed25519.pub
 chmod 600 ~/.ssh/authorized_keys
 sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+set +e
+apk add --no-cache python || yum install -y python || apt-get update && apt-get install -y python
